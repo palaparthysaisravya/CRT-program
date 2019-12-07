cd62555d7f9a41b10f155d1be2780434a7443c6c
palaparthysaisravya <58398129+palaparthysaisravya@users.noreply.github.com>
2019-12-07 11:16:31 +0530

    Add files via upload

---
+++ program 1.cpp
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the values of a,b");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\n the sum is : %d",c); 
+    return 0;
+}
+
